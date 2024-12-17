61aa7662e529391f899c085877760edb71368271
RidaAbid7 <ridaabid767@gmail.com>
2024-12-17 21:32:52 +0500

    moderators_profile

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import ComplianceServices from '../components/ComplianceServices';
 import ComplianceSearches from '../components/complianceSearches';
 import ComplianceMonitoring from '@/app/complianceMonitoring/page';
 import DocumentVerification from './documentVerification/page';
+import ModeratorPage from '@/app/practice/page';
 
 export default function Home() {
   return (
@@ -11,7 +12,7 @@ export default function Home() {
     <div className="flex justify-center w-full py-2 px-2">
             <div className="w-full max-w-3xl">
               {/* <ComplianceMonitoring/> */}
-              <DocumentVerification/>
+              <ModeratorPage/>
             </div>
           </div>
     </>
